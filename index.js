82d26c8a6fbdcb6f79d0e2f0901e9e4ea24c8db5
Anthony Febus <febus.anthony@gmail.com>
2018-08-09 19:42:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ if (name === "Susan") {
   console.log("The name is correct")
 }
 
-if (height === "74") {
+if (height === "70") {
   console.log("The height is correct")
 }
 // Don't worry about this
